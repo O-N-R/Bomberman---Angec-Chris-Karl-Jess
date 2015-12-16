936b400ee33fae1b11de1b22f80cd72f740942a7
jcmangaliag <jmmangaliag@up.edu.ph>
2015-12-16 21:48:18 +0800

    Added BomberMan from Section X

---
+++ BomberMain.java
@@ -0,0 +1,18 @@
+/*
+CMSC 22 Project: Bomberman
+
+Carrasco, Angelica Grace		X-4L
+Juruena, Christine Mae			X-1L
+Pios, Karl Jasson				X-4L
+Valdez, Jessica Ria				X-1L
+*/
+
+public class BomberMain{
+	
+	public static void main(String[] args){
+
+		BomberMenu frame = new BomberMenu();
+
+	}
+
+}
